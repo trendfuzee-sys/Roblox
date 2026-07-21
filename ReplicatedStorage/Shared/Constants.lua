2c492654d5c3b3bb756adc24cf242cb0810f9d05
trendfuzee-sys <trendfuzee@gmail.com>
2026-07-21 22:43:37 +0300

    Add shared constants module

---
+++ ReplicatedStorage/Shared/Constants.lua
@@ -0,0 +1,8 @@
+local Constants = {}
+
+Constants.MAX_LEVEL = 100
+Constants.LEAGUES = {"Bronze","Silver","Gold","Platinum","Diamond","Master","Grandmaster","Legend","Immortal"}
+Constants.DEFAULT_WALKSPEED = 16
+Constants.DEFAULT_MAX_HEALTH = 100
+
+return Constants
