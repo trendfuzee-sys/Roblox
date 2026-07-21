bfc6dadd620ef3f95f864f70a07bacba43e43b6d
trendfuzee-sys <trendfuzee@gmail.com>
2026-07-21 22:44:11 +0300

    Add shared config

---
+++ ReplicatedStorage/Shared/Config.lua
@@ -0,0 +1,10 @@
+local Config = {}
+
+Config.StreamingEnabled = true
+Config.ServerAuthoritativeCombat = true
+Config.MaxPlayers = 50
+Config.StartMoney = 100
+Config.StartMana = 100
+Config.StartXP = 0
+
+return Config
